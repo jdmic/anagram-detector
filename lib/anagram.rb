d78f068d8b60bf9abe45b02707acf59a50922b39
James <james.michalec58@gmail.com>
2021-01-24 12:45:48 -0800

    commit tree ed98d35def98bcb0383f322877cda6a90941ed71

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,25 @@
 # Your code goes here!
+require "pry"
+class Anagram
+
+    attr_accessor :anagram
+
+    def initialize(anagram)
+        @anagram = anagram
+    end
+
+    def match(words)
+        answer = []
+
+        split_sorted_anagram = @anagram.split("").sort
+        
+         words.each do |string|
+            split_sorted_string = string.split("").sort
+    
+            if split_sorted_string == split_sorted_anagram
+                answer << string
+            end
+        end
+        answer     
+    end
+end
